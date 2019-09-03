75e329cb5fcfae4c93e2501ff943dccac77ef622
Yuki Aoki <20838151+yuki-js@users.noreply.github.com>
2019-09-03 10:35:37 +0900

    include ripple protob

--- legacy/firmware/fsm.h
+++ legacy/firmware/fsm.h
@@ -28,6 +28,7 @@
 #include "messages-management.pb.h"
 #include "messages-nem.pb.h"
 #include "messages-stellar.pb.h"
+#include "messages-ripple.pb.h"
 
 // message functions
 
@@ -133,4 +134,7 @@ void fsm_msgStellarAccountMergeOp(const StellarAccountMergeOp *msg);
 void fsm_msgStellarManageDataOp(const StellarManageDataOp *msg);
 void fsm_msgStellarBumpSequenceOp(const StellarBumpSequenceOp *msg);
 
+// ripple
+void fsm_msgRippleGetAddress(const RippleGetAddress *msg);
+void fsm_msgRippleSignTx(const RippleSignTx *msg);
 #endif
